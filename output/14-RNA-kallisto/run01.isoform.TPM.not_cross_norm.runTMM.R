60af5e172ea4cfee37edf5c0c643775ebc3d638d
Steven Roberts <sr320@uw.edu>
2022-06-21 09:27:02 -0700

    kallisto

---
+++ output/14-RNA-kallisto/run01.isoform.TPM.not_cross_norm.runTMM.R
@@ -0,0 +1,9 @@
+library(edgeR)
+
+rnaseqMatrix = read.table("../output/14-RNA-kallisto/run01.isoform.TPM.not_cross_norm", header=T, row.names=1, com='', check.names=F)
+rnaseqMatrix = as.matrix(rnaseqMatrix)
+rnaseqMatrix = round(rnaseqMatrix)
+exp_study = DGEList(counts=rnaseqMatrix, group=factor(colnames(rnaseqMatrix)))
+exp_study = calcNormFactors(exp_study)
+exp_study$samples$eff.lib.size = exp_study$samples$lib.size * exp_study$samples$norm.factors
+write.table(exp_study$samples, file="../output/14-RNA-kallisto/run01.isoform.TPM.not_cross_norm.TMM_info.txt", quote=F, sep="\t", row.names=F)
